183857e3a4da93fde2e3ffa813f3d216229e44fb
paolo flore <paoloflore937@gmail.com>
2023-12-21 23:48:34 +0100

    variabbili

--- Main.java
+++ Main.java
@@ -4,11 +4,40 @@ public class Main {
         //manda a capo anmche con il  .println
         System.out.println("funziona ? \nnuova riga ");
         System.out.println("funziona ? \nnuova riga ");
+
+        System.out.print("funziona ? nuova riga ");
+
+
+        //variabili 
+        //cosa sono le variabbili ?
+        //la variabbile x puo contenere un valore come = 8 
+        //la variabbile quindi la possiamo riutilizzare  . quindi la variabbile e un contenitore di valori 
+
+        
+        //qui dichiaro la variabbile 
+        int x;
+
+        //asegnazione variabbile
+        x = 1230;
+
+        //oppre la dichiaro e la asegno allo stesso tempo
+        int y = 20;
+
+        //variabbile stringa
+        String w = "stinga";
+
+        //
+        double z =  10.2;
+
+
+        System.out.println(x);
+        System.out.println(w);
+        System.out.println(y);
+        System.out.println(z);
+
     }
 }
 
-// public class main {
-//     public static void main(String[] args) {
-//         System.out.println("x");
-//     }
-// }
+
+
+
