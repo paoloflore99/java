f473500dc4ca1d3b038fb767e0a571481ae4dee0
paolo flore <paoloflore937@gmail.com>
2023-12-21 23:14:36 +0100

    funzione

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        //con il \n manda a capoi
+        System.out.println("funziona ? \nnuova riga ");
+        System.out.println("funziona ? \nnuova riga ");
+    }
+}
+
+// public class main {
+//     public static void main(String[] args) {
+//         System.out.println("x");
+//     }
+// }
